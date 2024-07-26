8bf0fc67127471e34a731858c8f848d95fc23549
Deepak-Aki <im.deepak1407@gmail.com>
2024-07-26 14:33:51 +0530

    Major Update!! 1. Most importantly!! any and all functions looping back to main menu after completion was fix.
    2. Dedicated menu function was removed and main menu call back was achieved within main method.
    
    3. File stream library is added and login credentials are stored into a .txt file named Credentials.
    
    4. login function is completed with very minor updates left.
    
    5.login works as expected!
    
    6. login function is called right after registration ends.

--- Assignment_Trail2.cpp
+++ Assignment_Trail2.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int opt=0;
 int DOB;
 string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
 
-void Register_Player() {
 
+void PID() {
+    //Generating Unique Player ID
+}
+
+void Register_Player() {
 
     cout<<"\n==== Player Registration ====\n\n";
     cout<<"Please Enter your details below\n\n";
-    cout<<"Enter your First Name: ";
+    cout<<"1. Enter your First Name: ";
     cin>>Fname;
     cout<<"\n";
 
-    cout<<"Enter your Last Name: ";
+    cout<<"2. Enter your Last Name: ";
     cin>>Lname;
     cout<<"\n";
 
     //using a while loop to validate Date of birth
     while (true) {
-        cout<<"Enter Your Date of Birth (DDMMYYYY): ";
+        cout<<"3. Enter Your Date of Birth (DDMMYYYY): ";
         cin>>DOB;
         if (cin.fail()) {
             cerr<<"Please enter an integer for the date of birth.Try again"<<endl;
@@ -32,97 +37,143 @@ void Register_Player() {
     }
     cout<<"\n";
 
-    cout<<"Please choose your username: ";
+    cout<<"4. Please choose your username: ";
     cin>>Uname;
     cout<<"\n";
 
-    cout<<"Please choose your password: ";
+    cout<<"5. Please choose your password: ";
     cin>>Pass;
 
-    cout<<"\n\n"<<"Please Make Sure to Remember Your Username and Password.\n";
+    cout<<"\n\n";
+
+    PID();
 
     {
-        // Generating a unique player ID
+        ofstream AuthSave ("Credentials.txt", ios::app); //Saving login details into Credentials.txt
+        AuthSave<<Uname<<"\t"<<Pass<<endl;
 
-        cout<<"Your Player ID is: ";;
+        cout<<"Profile created successfully.\n"<<"Please Make Sure to Remember Your Username and Password.\n"<<endl;
+        AuthSave.close();
+
     }
 
-    cout<<"\n\n";
+     cout<<"\n\n";
+
     system("pause");
     system("cls");
+
 }
 
-void menu() {
-    while(true) {
-    cout<<":::::::: Upcountry Warriors Registration Page ::::::::\n"<<endl;
-    cout<<"Select an option below:\n"<<endl;
+void login() {
 
-    cout<<"1. Registration as a Player"<<endl;
-    cout<<"2. Registration as a Manager/Couch"<<endl;
-    cout<<"3. Login"<<endl;
-    cout<<"4. Exit\n"<<endl;
+    string UN, PW,USR,PWD;
+    bool counter=false;
+    cout<<"\n==== Login Page ====\n\n";
+    cout<<"Please Enter your Username and Password\n\n";
 
-    cout<<"Please enter your choice:  ";
+    cout<<"1. Enter your Username: ";
+    cin>>USR;
+    cout<<"\n";
 
-
-
-    cin>>opt;
-    if (cin.fail() || opt>4 || opt<1){
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cout<<"Please choice one of the given options."<<endl;
-        system("pause");
-        system ("cls");
-    }
-    else {break;}
-}
-
-    system("cls");
-}
-
-int main() {
-    menu();//Calling the menu function to display the options. For the first time.
-
-
-    switch(opt) {
-        case 1:
-
-            system("cls");
-        Register_Player(); //This function registers the players
-        menu();//This loops back to the main menu after registration.
-
-        break;
-
-
-        case 2:
-            cout<<"Still Under Development."<<endl;
-        cout<<"Manager/Couch Registration Sucessful!"<<endl;
-        // Register_MC(); Register Mc will be the function to register manager/couch.
-        menu();
-        break;
-
-
-        case 3:
-            cout<<"    Login Page    \n";
-        // Login(); Login will be the function to approve user permission.
-        break;
-
-
-        case 4:
-            cout<<"Exiting the Program...\n";
-        break;
+    cout<<"2. Enter your Password: ";
+    cin>>PWD;
 
 
 
-        default:
-        {
-            menu();
+    ifstream Auth;
+    Auth.open("Credentials.txt", ios::in);
+    if(!Auth.is_open()) {cerr<<"File doesn't exist/couldn't be opened...";}
+
+    //Auth.seekg(8,ios::cur); //The file pointer reads from the 8th position.
+
+
+    while(Auth>>UN>>PW) {
+        if(USR==UN && PW==PWD) {
+            counter = true;
+            break;
+        }
+        else if (USR==UN && PWD=="Admin") {
+            counter=true;
+            cout<<"Welcome Admin"<<endl;
+            break;
         }
 
 
 
 
-        system("pause");
-        return 0;
-    }
+    }Auth.close();
+        if(!counter==true) {
+            cerr<<"\n\nInvalid Username or Password!\nPlease Try Again"<<endl;
+
+        }
+        else {
+
+            cout<<"\nLogin Successful!"<<endl;
+            // Player_Profile();
+        }
+
+
+
+
+    cout<<"\n\n";
+    system("pause");
+    system("cls");
+
 }
+
+int main() {
+
+    //Running the main menu on a loop until the user chooses to exit.
+    while(true) {
+        cout<<":::::::: Upcountry Warriors Registration Page ::::::::\n"<<endl;
+        cout<<"Select an option below:\n"<<endl;
+
+        cout<<"1. Registration as a Player"<<endl;
+        cout<<"2. Registration as a Manager/Couch"<<endl;
+        cout<<"3. Login"<<endl;
+        cout<<"4. Exit\n"<<endl;
+
+        cout<<"Please enter your choice:  ";
+        cin>>opt;
+
+
+
+        switch(opt) {
+            case 1:
+                system("cls");
+                Register_Player(); //This function registers the players
+                login();
+            break;
+
+
+            case 2:
+                system("cls");
+                cout<<"Still Under Development."<<endl;
+                cout<<"Manager/Couch Registration Sucessful!"<<endl;
+            // Register_MC(); Register Mc will be the function to register manager/couch.
+
+                break;
+            case 3:
+                system("cls");
+                login(); //Login will be the function to approve user permission.
+            break;
+
+
+            case 4:
+                cout<<"Exiting the Program...\n";
+            system("pause");
+            return 0;
+
+            default: {
+                if (cin.fail() || opt>4 || opt<1){
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout<<"Please choice one of the given options."<<endl;
+                    system("pause");
+                    system ("cls");
+                }
+            }
+
+            }
+        }
+    }
