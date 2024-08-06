d898d406db4fa061c9507d07d3c9a3838d2a8f85
Aki-1407 <im.deepak1407@gmail.com>
2024-08-07 02:19:27 +0530

    Cleaned the code to look more tidy

--- FIP/Assignment.cpp
+++ FIP/Assignment.cpp
@@ -6,101 +6,31 @@ using namespace std;
 int opt=0,opt1=0;
 int DOB;
 double runs;
-string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
+string Fname,Lname,Uname,Pass,Team1,Team2,Pid; //Player Registeration variables.
+string UN, PW,USR,PWD; //Login variables.
+
+    void login(); //Login will be the function to approve user permission.
+
+    void PID(); //Generating Unique Player
+
+    void userReg(); //This function registers Users.
+
+    void dob(); //Date of Birth function which loops until it gets a interger value
+
+    void Register_Player(); //This function is called when the user wants to add player details to database
+
+    void User_Menu(); //This function will be called when user logs in.
+
+    void Forgot_Password(); //This function is called when user forgets their password.
+
+    void Menu(); //Main menu
 
 
-    void login();
-
-    void PID() {
-        //Generating Unique Player
-                }
-
-    void userReg(){
-
-    cout<<"\n----- Registration Page -----\n\n";
-
-    cout<<"4. Please choose your username: ";
-    cin>>Uname;
-    cout<<"\n";
-
-    cout<<"5. Please choose your password: ";
-    cin>>Pass;
-
-        //Saving login details into Credentials.txt
-        {
-        ofstream AuthSave ("Credentials.txt", ios::app);
-        AuthSave<<Uname<<"\t"<<Pass<<endl;
-
-        cout<<"\n\nAccount Registered Successfully.\n"<<endl;
-        AuthSave.close();
-
-        system("pause");
-        system("cls");
-
-        }
-}
-
-    void dob() {while (true) {
-    cout<<"3. Enter player Date of Birth (DDMMYYYY): ";
-    cin>>DOB;
-    if (cin.fail()) {
-        cerr<<"Please enter an integer for the date of birth.Try again"<<endl;
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cerr.clear();
+    int main() {
+        
+        Menu();
+        return 0;
     }
-    else { break; }
-}}
-
-
-
-    void Register_Player() {
-
-        cout<<"\n==== Player Registration ====\n\n";
-        cout<<"Please Enter player details below\n\n";
-        cout<<"1. Enter player First Name: ";
-        cin>>Fname;
-        cout<<"\n";
-
-        cout<<"2. Enter player Last Name: ";
-        cin>>Lname;
-        cout<<"\n";
-
-        //using a while loop to validate Date of birth
-        /*while (true) {
-            cout<<"3. Enter player Date of Birth (DDMMYYYY): ";
-            cin>>DOB;
-            if (cin.fail()) {
-                cerr<<"Please enter an integer for the date of birth.Try again"<<endl;
-                cin.clear();
-                cin.ignore(1000, '\n');
-            }
-            else { break; }
-        }*/
-        dob();
-
-        cout<<"\n\n"<<"Profile created successfully.\n"<<endl;
-
-        PID();
-
-        //Saving User Details into Database.txt
-        {
-            string data[8] = {Pid,Fname,Lname,to_string(DOB)};
-            ofstream DataSave ("Database.txt", ios::app);
-            DataSave<<data[0]<<"\t"<<data[1]<<"\t"<<data[2]<<"\t"<<data[3]<<endl;
-            cout<<"User details saved successfully.\n\n";
-            DataSave.close();
-        }
-
-        cout<<"\n\n";
-
-        system("pause");
-        system("cls");
-
-    }
-
-    void User_Menu();
-    void Forgot_Password();
 
     void Menu (){
             //Running the main menu on a loop until the user chooses to exit.
@@ -121,7 +51,7 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
                     case 1:
 
                         system("cls");
-                    userReg(); //This function registers Users.
+                    userReg(); 
                     Menu();
 
                     break;
@@ -129,20 +59,21 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
                     case 2:
 
                         system("cls");
-                    login(); //Login will be the function to approve user permission.
-                    User_Menu(); //This function will be called when user logs in.
+                    login(); 
+                    User_Menu(); 
 
                     break;
 
                     case 3:
                         system("cls");
-                        Forgot_Password(); //This function is called when user forgets their password.
+                        Forgot_Password(); 
                         Menu();
 
                     break;
 
                     case 4:
-                        cout<<"Exiting the Program...\n";
+                    system("cls");
+                        cout<<"Exiting the Program...\nThank You\n";
                     system("pause");
                     exit(0);
 
@@ -159,72 +90,32 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
             }
         }
 
-            void Forgot_Password() {
-        string UNm,UNm_i,Pswd;
-        bool check=false;
-        cout<<"---Forgot Your Password ?..---\n\n";
-        cout<<"Please enter your usernanme: ";
-        cin>>UNm_i;
-        
-        
-        
+        void userReg(){
 
-        ifstream forg;
-        forg.open("Credentials.txt");
+    cout<<"\n----- Registration Page -----\n\n";
 
+    cout<<"4. Please choose your username: ";
+    cin>>Uname;
+    cout<<"\n";
 
-        while(forg>>UNm>>Pswd) {
-            if (UNm_i==UNm) {
-                check=true;
-                break;
-            }
-            } forg.close();
+    cout<<"5. Please choose your password: ";
+    cin>>Pass;
 
-           
+        //Saving login details into Credentials.txt
+        {string Cred[2]={Uname,Pass};
+        ofstream AuthSave ("Credentials.txt", ios::app);
+        AuthSave<<Cred[0]<<"\t"<<Cred[1]<<endl;
 
-        
+        cout<<"\n\nAccount Registered Successfully.\n"<<endl;
+        AuthSave.close();
 
-        if(check==true) {
-            cout<<"\n\nYour Password is: "<<Pswd<<endl;
-            }
+        system("pause");
+        system("cls");
 
-             else {
-                
-                cout << "\n\nUsername not found.\nPlease Register. \n\n";
-                cout<<UNm<<UNm_i;
-                
-                system ("Pause");
-                system("cls");
-                }  
-
-        cout<<"\n\n";
-
-        int inp;
-                
-                cout<<"1. Register Account\n";
-                cout<<"2. Main Menu\n\n";
-                cout<<"Option: ";
-                cin>>inp;
-                if (inp==1){system("cls");
-                userReg();}
-
-                else if (inp==2){
-                system("cls");
-                cout<<"Returning to Main Menu...\n\n";
-                system("Pause");
-                
-                Menu();}
+        }
 }
 
-    int main() {
-
-        Menu();
-        return 0;
-    }
-
-    void login() {
-
-        string UN, PW,USR,PWD;
+        void login() {
 
         bool counter=false;
         cout<<"\n==== Login Page ====\n\n";
@@ -237,12 +128,9 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
         cout<<"2. Enter your Password: ";
         cin>>PWD;
 
-
-
-
         ifstream Auth;
         Auth.open("Credentials.txt", ios::in);
-        if(!Auth.is_open()) {cerr<<"File doesn't exist/couldn't be opened...";}
+        if(!Auth.is_open()) {cerr<<"\n\nFile doesn't exist/couldn't be opened...";}
 
 
         while(Auth>>UN>>PW) {
@@ -254,25 +142,21 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
             }
 
 
-
         }Auth.close();
         if(!counter==true) {
-            cerr<<"\n\nInvalid Username or Password!\nPlease Try Again or Use Forgot Password."<<endl;
-             cout<<UN<<PWD<<"\n\n";
+            cerr<<"\n\nInvalid Username or Password!\nPlease Try Again or Use Forgot Password.\n"<<endl;
             system("pause");
             system("cls");
             Menu();
         }
-
-
     }
 
-    void User_Menu() {
+            void User_Menu() {
 
         cout<<"\nLogin Successful!\n\n"<<endl;
         for(;;){
 
-            cout<<"\t\tWelcome [ "<<Uname<<" ]"<<"\n\n";
+            cout<<"\t\tWelcome [ "<<USR<<" ]"<<"\n\n";
             cout<<"1. Add Player\n";
             cout<<"2. Search Player\n";
             cout<<"3. Change Team\n";
@@ -316,7 +200,7 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
 
                 case 6:
                     system("cls");
-                cout<<"Exit";
+                cout<<"Leaving...\nThank You\n";
                 system("pause");
                 exit(0);
 
@@ -332,3 +216,106 @@ string Fname,Lname,Uname,Pass,Team1,Team2,Pid;
             }
         }
         }
+
+                void Register_Player() {
+
+        cout<<"\n==== Player Registration ====\n\n";
+        cout<<"Please Enter player details below\n\n";
+        cout<<"1. Enter player First Name: ";
+        cin>>Fname;
+        cout<<"\n";
+
+        cout<<"2. Enter player Last Name: ";
+        cin>>Lname;
+        cout<<"\n";
+
+       
+        dob();
+
+        cout<<"\n\n"<<"Profile created successfully.\n"<<endl;
+
+        PID();
+
+        //Saving User Details into Database.txt
+        {
+            string data[8] = {Pid,Fname,Lname,to_string(DOB)};
+            ofstream DataSave ("Database.txt", ios::app);
+            DataSave<<data[0]<<"\t"<<data[1]<<"\t"<<data[2]<<"\t"<<data[3]<<endl;
+            cout<<"User details saved successfully.\n\n";
+            DataSave.close();
+        }
+
+        cout<<"\n\n";
+
+        system("pause");
+        system("cls");
+
+    }
+
+                    void dob() {while (true) {
+    cout<<"3. Enter player Date of Birth (DDMMYYYY): ";
+    cin>>DOB;
+    if (cin.fail()) {
+        cerr<<"Please enter an integer for the date of birth.Try again"<<endl;
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+    else { break; }
+}}
+
+                    void PID() { }
+
+          void Forgot_Password() {
+
+        string UNm,UNm_i,Pswd;
+        bool check=false;
+        cout<<"---Forgot Your Password ?..---\n\n";
+        cout<<"Please enter your usernanme: ";
+        cin>>UNm_i;
+        
+        ifstream forg;
+        forg.open("Credentials.txt");
+
+        while(forg>>UNm>>Pswd) {
+            
+            if (UNm_i==UNm) {
+                check=true;
+                break;
+            }
+            } forg.close();
+
+        if(check==true) {
+            cout<<"\n\nYour Password is: "<<Pswd<<endl;
+            }
+
+        else { 
+            cout << "\n\nUsername not found.\nPlease Register.";
+            }  
+
+        cout<<"\n\n";
+
+        int inp;
+                
+                cout<<"1. Register Account\n";
+                cout<<"2. Main Menu\n";
+                cout<<"3. Try Again\n\n";
+                cout<<"Option: ";
+                cin>>inp;
+                if (inp==1){system("cls");
+                userReg();}
+
+                else if (inp==2){
+                system("cls");
+                cout<<"Returning to Main Menu...\n\n";
+                system("Pause");
+                
+                Menu();}
+
+                else if (inp==3){
+                    system("cls");
+                    Forgot_Password();
+                }
+}
+
+    
+      
